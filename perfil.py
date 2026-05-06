d690da86e5a0f114f657f8e0ced21aedbd0f6514
lucasmarques201421-ctrl <lucasmarques201421@gmail.com>
2026-05-06 19:51:43 -0300

    Add profession variable and print statement

--- perfil.py
+++ perfil.py
@@ -1,7 +1,9 @@
 nome = "Lucas Marques"
 idade = 32
 cidade = "Canoas"
+profissao = "Estudante de TI"
 
 print("Nome:", nome)
 print("Idade:", idade)
 print("Cidade:", cidade)
+print("Profissão:", profissao)
